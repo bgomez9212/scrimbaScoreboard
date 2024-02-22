fb4898e36e54112adfb0658a3a8b303a0e791c33
Brandon Gomez <bgomez9212@gmail.com>
2024-02-22 14:45:48 -0800

    some edits

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-let homeScore = 0
-let homeEl = document.getElementById('homescore-el')
-function homeOne(){
-    homeScore += 1
-    homeEl.textContent = homeScore
-}
-function homeTwo(){
-    homeScore += 2
-    homeEl.textContent = homeScore
-}
-function homeThree(){
-    homeScore += 3
-    homeEl.textContent = homeScore
+let homeScore = 0;
+let homeEl = document.getElementById("homescore");
+
+function addHomeScore(value) {
+  homeScore += parseInt(value);
+  homeEl.textContent = homeScore;
 }
 
-let guestScore = 0
-let guestEl = document.getElementById('guestscore-el')
-function guestOne(){
-    guestScore += 1
-    guestEl.textContent = guestScore
+let awayScore = 0;
+let awayEl = document.getElementById("awayscore");
+
+function addAwayScore(value) {
+  awayScore += parseInt(value);
+  awayEl.textContent = awayScore;
 }
-function guestTwo(){
-    guestScore += 2
-    guestEl.textContent = guestScore
-}
-function guestThree(){
-    guestScore += 3
-    guestEl.textContent = guestScore
-}
+
+let endGame = document.getElementById("endgame");
+
+endGame.addEventListener("click", function () {
+  document.getElementById("end-screen").style.display = "flex";
+  document.getElementById("container").style.display = "none";
+  let buttons = document.getElementsByClassName("scoring");
+  for (button of buttons) {
+    button.classList.add("hidden");
+  }
+  if (homeScore > awayScore) {
+    document.getElementById("winning-message").innerText = `
+        HOME TEAM WINS`;
+  } else if (awayScore > homeScore) {
+    document.getElementById("winning-message").innerText = `
+        AWAY TEAM WINS`;
+  } else {
+    document.getElementById("winning-message").innerText = `
+        DRAW`;
+  }
+});
+
+let startGame = document.getElementById("newGame");
+
+startGame.addEventListener("click", function () {
+  document.getElementById("end-screen").style.display = "none";
+  document.getElementById("container").style.display = "flex";
+  let buttons = document.getElementsByClassName("scoring");
+  for (button of buttons) {
+    button.classList.remove("hidden");
+  }
+  homeScore = 0;
+  awayScore = 0;
+  homeEl.textContent = homeScore;
+  awayEl.textContent = awayScore;
+});
